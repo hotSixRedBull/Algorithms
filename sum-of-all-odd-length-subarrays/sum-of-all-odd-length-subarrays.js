7dd717eedf8351e8899bd2d5c69649da197511dd
Yi Seunggi <akkalee1207@gmail.com>
2021-09-17 21:16:07 +0900

    Time: 60 ms (100.00%), Space: 39.2 MB (49.24%) - LeetHub

--- sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.js
+++ sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.js
@@ -3,18 +3,9 @@
  * @return {number}
  */
 var sumOddLengthSubarrays = function(arr) {
-    let res = 0;
-    for (let len=1; len<arr.length+1; len+=2) {
-        let cur = 0;
-        for (let i=0; i<len; i++) {
-            cur += arr[i];
-        }
-        res += cur;
-        for (let i=len; i<arr.length; i++) {
-            cur -= arr[i-len];
-            cur += arr[i];
-            res += cur;
-        }
+    let res = 0, n = arr.length;
+    for (let i = 0; i < n; i++) {
+        res += Math.floor(((i+1)*(n-i)+1)/2)* arr[i];
     }
     return res;
 };

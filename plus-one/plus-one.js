160a5a1642ff57e104edfba466543dd812fee989
Yi Seunggi <akkalee1207@gmail.com>
2021-09-22 15:28:23 +0900

    Time: 76 ms (56.93%), Space: 38.8 MB (52.47%) - LeetHub

---
+++ plus-one/plus-one.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let carry = 1;
+    for (let i=digits.length-1;i > -1;i--) {
+        let cur = digits[i];
+        let calc = cur + carry;
+        carry = 0;
+        if (calc > 9) {
+            calc = 0;
+            carry = 1;
+        }
+        digits[i] = calc;
+    }
+    if (carry == 1) {
+        digits.unshift(1);
+    }
+    return digits;
+};

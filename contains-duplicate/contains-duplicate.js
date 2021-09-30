7a504cf1165dd48ec9ac9812ad5e0f4b861dd3d1
Yi Seunggi <akkalee1207@gmail.com>
2021-09-30 20:28:50 +0900

    Time: 88 ms (62.56%), Space: 47.1 MB (15.68%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let map = {};
+    for (let i=0; i<nums.length; i++) {
+        if (!!map[nums[i]]) {
+            return true;
+        }
+        map[nums[i]] = 1;
+    }
+    return false;
+};

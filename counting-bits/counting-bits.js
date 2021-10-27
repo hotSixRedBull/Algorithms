ae56bb981176cae7d0b200db0a5beeb3243e75ec
Yi Seunggi <akkalee1207@gmail.com>
2021-10-27 22:24:57 +0900

    Time: 84 ms (98.95%), Space: 44.3 MB (93.90%) - LeetHub

--- counting-bits/counting-bits.js
+++ counting-bits/counting-bits.js
@@ -3,27 +3,9 @@
  * @return {number[]}
  */
 var countBits = function(n) {
-    let ans = [];
-    function countOneBit(num) {
-        let copy = num;
-        let ans = 0;
-        while (copy > 0) {
-            if (copy % 2 == 1) {
-                ans++;
-                copy -= 1;
-            }
-            copy /= 2;
-        }
-        return ans;
-    }
-    for (let i=0; i<=n; i++) {
-        ans.push(countOneBit(i));
+    let ans = Array(n+1).fill(0);
+    for (let i=1; i<=n; i++) {
+        ans[i] = ans[i&(i-1)] + 1;
     }
     return ans;
 };
-    
-/*
-5 = 2*2 + 1
-2 = 2*1 + 0
-1 = 2*0 + 1
-*/

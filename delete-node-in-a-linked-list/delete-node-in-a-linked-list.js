73b8232d27c6619cab2d4a61c725630243ebf899
Yi Seunggi <akkalee1207@gmail.com>
2021-11-01 22:20:55 +0900

    Time: 88 ms (51.71%), Space: 40.4 MB (82.83%) - LeetHub

---
+++ delete-node-in-a-linked-list/delete-node-in-a-linked-list.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    let prev = null;
+    while (node.next != null) {
+        node.val = node.next.val;
+        prev = node;
+        node = node.next;
+    }
+    prev.next = null;
+};

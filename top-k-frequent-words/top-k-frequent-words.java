c181fe1934650d9e48ffcf68f9f71bd3af6e4265
Yi Seunggi <akkalee1207@gmail.com>
2021-12-18 22:45:00 +0900

    Time: 7 ms (34.06%), Space: 42.1 MB (16.34%) - LeetHub

--- top-k-frequent-words/top-k-frequent-words.java
+++ top-k-frequent-words/top-k-frequent-words.java
@@ -1,35 +1,30 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        HashMap<String, Integer> map = new HashMap<>();
-        HashMap<String, Integer> pqMap = new HashMap<>();
-        ArrayList<String> pq = new ArrayList<>();
-        for (String word : words) {
-            int freq = 1;
-            if (map.containsKey(word)) {
-                freq = map.get(word)+1;
-                map.put(word, freq);
-            }
-            else {
-                map.put(word, 1);
-            }
-        }
-        //System.out.println(map);
-        List<Map.Entry<String, Integer>> mapEntry = new ArrayList<>(map.entrySet());
-        Collections.sort(mapEntry, new Comparator<Map.Entry<String, Integer>>() {
-            // compare로 값을 비교
-			public int compare(Map.Entry<String, Integer> obj1, Map.Entry<String, Integer> obj2) {
-				// 오름 차순 정렬
-                if (obj1.getValue() == obj2.getValue()) {
-                    return obj1.getKey().compareTo(obj2.getKey());
-                }
-				return obj2.getValue().compareTo(obj1.getValue());
-			}
-        });
-        //System.out.println(mapEntry);
         
-        for (int i=0; i<k; i++) {
-            pq.add(mapEntry.get(i).getKey());
+        List<String> result = new LinkedList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<words.length; i++)
+        {
+            if(map.containsKey(words[i]))
+                map.put(words[i], map.get(words[i])+1);
+            else
+                map.put(words[i], 1);
         }
-        return pq;
+        
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+                 (a,b) -> a.getValue()==b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
+        );
+        
+        for(Map.Entry<String, Integer> entry: map.entrySet())
+        {
+            pq.offer(entry);
+            if(pq.size()>k)
+                pq.poll();
+        }
+
+        while(!pq.isEmpty())
+            result.add(0, pq.poll().getKey());
+        
+        return result;
     }
 }

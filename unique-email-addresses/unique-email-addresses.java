803690688f809135a33b7fc70ffdd7401b386269
Yi Seunggi <akkalee1207@gmail.com>
2022-01-02 22:59:14 +0900

    Time: 24 ms (34.07%), Space: 42.7 MB (21.43%) - LeetHub

---
+++ unique-email-addresses/unique-email-addresses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
+        for (String email : emails) {
+            StringBuilder sb = new StringBuilder();
+            int index = 0;
+            //local name
+            for (; index<email.length(); index++) {
+                if (email.charAt(index) == '+'
+                    || email.charAt(index) == '@') {
+                    break;
+                }
+                else if (email.charAt(index) == '.') {
+                    continue;
+                }
+                sb.append(email.charAt(index));
+            }
+            while (email.charAt(index) != '@') {
+                index++;
+            }
+            //domain
+            for (; index<email.length(); index++) {
+                sb.append(email.charAt(index));
+            }
+            set.add(sb.toString());
+            //System.out.println(sb.toString());
+        }
+        return set.size();
+    }
+}

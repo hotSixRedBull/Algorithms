ec01d5332bbdca2f11e73b1ca89e03cdb097fcef
Yi Seunggi <akkalee1207@gmail.com>
2022-06-13 21:13:56 +0900

    Time: 6 ms (60.25%), Space: 40.2 MB (71.75%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.java
@@ -1,25 +1,9 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        HashMap<Integer, Long> map = new HashMap();
-        for (long i=0; i<=c; i++) {
-            long square = i*i;
-            if (square > c) {
-                break;
-            }
-            map.put((int)i, square);
-        }
-        int left = 0;
-        int right = map.size()-1;
-        while (left <= right) {
-            long sum = map.get(left) + map.get(right);
-            if (sum == c) {
+        for (long a=0; a*a<=c; a++) {
+            double b= Math.sqrt(c-a*a);
+            if (b == (int) b) {
                 return true;
-            }
-            if (sum < c) {
-                left++;
-            }
-            else {
-                right--;
             }
         }
         return false;

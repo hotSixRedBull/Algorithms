288d544812e9eb3d9ed6f7abf66c00e32a0c4a4f
Yi Seunggi <akkalee1207@gmail.com>
2022-06-14 09:40:03 +0900

    Time: 44 ms (28.14%), Space: 55.3 MB (28.17%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> answer = new ArrayList();
-        backtrack(0, new ArrayList(), n, k, answer);
-        return answer;
+        List<List<Integer>> ans = new ArrayList();
+        backtrack(0, new ArrayList(), ans, n, k);
+        return ans;
     }
-    public void backtrack(int pos, List<Integer> built, int n, int k, List<List<Integer>> ans) {
+    
+    public void backtrack(int pos, List<Integer> built, List<List<Integer>> ans, int n, int k) {
         if (pos == k) {
             ans.add(new ArrayList(built));
             return;
         }
-        int last = pos > 0 ? built.get(pos-1)+1 : 1;
+        int last = pos == 0 ? 1 : built.get(built.size()-1)+1;
         for (int i=last; i<=n; i++) {
             built.add(i);
-            backtrack(pos+1, built, n, k, ans);
-            built.remove(pos);
+            backtrack(pos+1, built, ans, n, k);
+            built.remove(built.size()-1);
         }
     }
 }

b01c3786b6cdac4c735cc54597275fcefa4cd8d8
Yi Seunggi <akkalee1207@gmail.com>
2022-12-27 19:51:16 +0900

    Time: 72 ms (95.76%), Space: 42.8 MB (76.59%) - LeetHub

---
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int connectSticks(int[] sticks) {
+        PriorityQueue<Integer> pq = new PriorityQueue();
+        for (int stick : sticks) {
+            pq.offer(stick);
+        }
+        int cost = 0;
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            cost += a + b;
+            pq.offer(a+b);
+        }
+        return cost;
+    }
+}

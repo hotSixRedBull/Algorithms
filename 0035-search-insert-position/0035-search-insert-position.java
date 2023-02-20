6842fe89b93858a2912eea47cbb8de1591538d2a
Yi Seunggi <akkalee1207@gmail.com>
2023-02-20 18:20:44 +0900

    Time: 0 ms (100.00%), Space: 42.1 MB (39.95%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,10 +1,22 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int pos = Arrays.binarySearch(nums, target);
-        if (pos < 0) {
-            pos++;
-            pos *= -1;
+        int lo = 0;
+        int hi = nums.length-1;
+        while (lo < hi) {
+            int mid = lo - (lo - hi)/2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                lo = mid+1;
+            }
+            else {
+                hi = mid;
+            }
         }
-        return pos;
+        if (nums[nums.length-1] < target) {
+            return nums.length;
+        }
+        return lo;
     }
 }

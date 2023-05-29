02bf261121a3cf35b1431ad7f25002be2569c133
Yi Seunggi <akkalee1207@gmail.com>
2023-05-29 16:17:18 +0900

    Time: 13 ms (10.37%), Space: 48.7 MB (5.19%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.java
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.java
@@ -9,32 +9,76 @@ class TicTacToe {
     public int move(int row, int col, int player) {
         map[row][col] = player;
         int[] streak = new int[4];
-        for (int i=0; i<n; i++) {
-            if (map[row][i] == player) {
-                streak[0]++;
-                if (streak[0] == n) {
-                    return player;
+        if (row == col) {
+            for (int i=0; i<n; i++) {
+                if (map[row][i] == player) {
+                    streak[0]++;
+                    if (streak[0] == n) {
+                        return player;
+                    }
                 }
-            }
-            if (map[i][col] == player) {
-                streak[1]++;
-                if (streak[1] == n) {
-                    return player;
+                if (map[i][col] == player) {
+                    streak[1]++;
+                    if (streak[1] == n) {
+                        return player;
+                    }
                 }
-            }
-            if (map[i][i] == player) {
-                streak[2]++;
-                if (streak[2] == n) {
-                    return player;
+                if (map[i][i] == player) {
+                    streak[2]++;
+                    if (streak[2] == n) {
+                        return player;
+                    }
                 }
-            }
-            if (map[i][n-1-i] == player) {
-                streak[3]++;
-                if (streak[3] == n) {
-                    return player;
+                if (map[i][n-1-i] == player) {
+                    streak[3]++;
+                    if (streak[3] == n) {
+                        return player;
+                    }
                 }
             }
         }
+        else if (row+col == n-1) {
+            for (int i=0; i<n; i++) {
+                if (map[row][i] == player) {
+                    streak[0]++;
+                    if (streak[0] == n) {
+                        return player;
+                    }
+                }
+                if (map[i][col] == player) {
+                    streak[1]++;
+                    if (streak[1] == n) {
+                        return player;
+                    }
+                }
+                if (map[i][n-1-i] == player) {
+                    streak[3]++;
+                    if (streak[3] == n) {
+                        return player;
+                    }
+                }
+            }
+        }
+        else {
+            for (int i=0; i<n; i++) {
+                if (map[row][i] == player) {
+                    streak[0]++;
+                    if (streak[0] == n) {
+                        return player;
+                    }
+                }
+                if (map[i][col] == player) {
+                    streak[1]++;
+                    if (streak[1] == n) {
+                        return player;
+                    }
+                }
+            }
+        }
+        // System.out.println(String.format(">>>>(%d, %d)", row, col));
+        // for (int r=0; r<n; r++) {
+        //     System.out.println(Arrays.toString(map[r]));
+        // }
         return 0;
     }
 }

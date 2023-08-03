776392752d1098bab4a7be80ebdb654924c1638d
Yi Seunggi <akkalee1207@gmail.com>
2023-08-03 23:50:21 +0900

    Time: 87 ms (6.22%), Space: 160.1 MB (5.78%) - LeetHub

--- 2361-minimum-costs-using-the-train-line/2361-minimum-costs-using-the-train-line.java
+++ 2361-minimum-costs-using-the-train-line/2361-minimum-costs-using-the-train-line.java
@@ -4,8 +4,9 @@ class Solution {
         int n = regular.length+1;
         long[] dists = new long[2*n+2];
         dists[0] = 0;
+        dists[n+1] = expressCost;
         for (int i=1; i<2*n+2; i++) {
-            dists[i] = Long.MAX_VALUE;
+            dists[i] = Integer.MAX_VALUE;
         }
         
         Map<Integer, Map<Integer, Long>> distMap = new HashMap();
@@ -25,37 +26,37 @@ class Solution {
         
         // System.out.println(distMap);
         
-        PriorityQueue<long[]> pq = new PriorityQueue<long[]>((a, b) -> (int)(a[2]-b[2]));
-        for (int nei : distMap.get(0).keySet()) {
-            pq.add(new long[]{0, nei, distMap.get(0).get(nei)});
-        }
+//         PriorityQueue<long[]> pq = new PriorityQueue<long[]>((a, b) -> (int)(a[2]-b[2]));
+//         for (int nei : distMap.get(0).keySet()) {
+//             pq.add(new long[]{0, nei, distMap.get(0).get(nei)});
+//         }
         
-        int[] used = new int[2*n+2];
-        while (pq.size() > 0) {
-            long[] cur = pq.poll();
-            // System.out.println(Arrays.toString(cur));
-            // System.out.println(Arrays.toString(dists));
-            int from = (int) cur[0];
-            int to = (int) cur[1];
-            long dist = cur[2];
+//         int[] used = new int[2*n+2];
+//         while (pq.size() > 0) {
+//             long[] cur = pq.poll();
+//             // System.out.println(Arrays.toString(cur));
+//             // System.out.println(Arrays.toString(dists));
+//             int from = (int) cur[0];
+//             int to = (int) cur[1];
+//             long dist = cur[2];
             
-            used[from]++;
+//             used[from]++;
             
-            if (dists[from]+dist < dists[to]) {
-                dists[to] = dists[from]+dist;
-            }
-            for (int nei : distMap.get(to).keySet()) {
-                if (used[nei] > 1) {
-                    continue;
-                }
-                pq.add(new long[]{cur[1], (long) nei, distMap.get(to).get(nei)});
-            }
-        }
+//             if (dists[from]+dist < dists[to]) {
+//                 dists[to] = dists[from]+dist;
+//             }
+//             for (int nei : distMap.get(to).keySet()) {
+//                 if (used[nei] > 1) {
+//                     continue;
+//                 }
+//                 pq.add(new long[]{cur[1], (long) nei, distMap.get(to).get(nei)});
+//             }
+//         }
         
         for (int i=0; i<regular.length; i++) {
-            dists[i+1] = Math.min(dists[i+1], dists[i]+regular[i]);
+            dists[i+1] = dists[i]+regular[i];
             dists[i+1] = Math.min(dists[i+1], dists[i+n+1]+regular[i]);
-            dists[i+n+2] = Math.min(dists[i+n+2], dists[i+n+1]+express[i]);
+            dists[i+n+2] = dists[i+n+1]+express[i];
             dists[i+n+2] = Math.min(dists[i+n+2], dists[i]+express[i]+expressCost);
         }
         

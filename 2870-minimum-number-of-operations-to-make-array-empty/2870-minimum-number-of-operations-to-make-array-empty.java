c51c0363941456def851e028c1c0b27453d44784
Yi Seunggi <akkalee1207@gmail.com>
2024-01-04 19:52:08 +0900

    Time: 32 ms (20.00%), Space: 57.9 MB (34.72%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        Map<Integer, Integer> cnt = new HashMap();
+        for (int num : nums) {
+            cnt.put(num, cnt.getOrDefault(num, 0)+1);
+        }
+        System.out.println(cnt);
+        
+        int min = 0;
+        for (int num : cnt.keySet()) {
+            int eachCnt = cnt.get(num);
+            // System.out.println(String.format("%d %% 3 == %d", eachCnt, eachCnt%3));
+            if (eachCnt % 3 == 0) {
+                min += eachCnt/3;
+            }
+            else if (eachCnt % 3 == 1) {
+                if (eachCnt == 1) {
+                    return -1;
+                }
+                // System.out.println(String.format("(int)Math.floor(%d/3) == %d", eachCnt, (int)Math.floor(eachCnt/3)));
+                min += (int) Math.floor(eachCnt/3) + 1;
+            }
+            else {
+                // System.out.println(String.format("(int)Math.floor(%d/3) == %d", eachCnt, (int)Math.floor(eachCnt/3)));
+                min += (int)Math.floor(eachCnt/3)+1;
+            }
+        }
+        return min;
+    }
+}
